3110b5a173e9b7d83cbc1360979240c11ad280a1
atlasfoo@pop <jf_mejiar547@live.com>
2021-07-24 23:02:35 -0600

    :heavy_plus_sign::wrench: - added drf simplejwt package, configured atuh create and auth create refresh actions

--- breve_drf_template/urls.py
+++ breve_drf_template/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from breve_drf_template.apps.core.urls import core_urls
 from breve_drf_template.apps.employee.urls import employee_urls
@@ -17,6 +18,8 @@ urlpatterns = [
     # set openapi docs as home
     path('', SpectacularRedocView.as_view(url_name='schema'), name='apidocs'),
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', TokenObtainPairView.as_view(), name='auth_get_token'),
+    path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh_token'),
     path('api/v1/', include(core_urls)),
     path('api/v1/', include(employee_urls)),
 ]
